7301b15f1373e651ed35aba5c1d6c56a9ba269cb
KevRiver <kanggakky1214@gmail.com>
2025-06-08 18:53:28 +0900

    fix(proto): fix option go_package

---
+++ script/generate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+root=$(git rev-parse --show-toplevel)
+
+function main() {
+    export PATH=${PATH}:${root}/ts/node_modules/.bin
+    pushd ${root}/go
+    [ -n "$(go env GOBIN)" ] && export PATH=${PATH}:$(go env GOBIN)
+    [ -n "$(go env GOPATH)" ] && export PATH=${PATH}:$(go env GOPATH)/bin
+    popd
+
+    pkgx buf lint
+    pkgx buf generate
+}
+
+main
